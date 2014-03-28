b12443581e421765f94b45da41d48f57c018f2b3
David <david.clares.rodriguez@gmail.com>
2014-03-28 13:02:07 +0100

    Añadido metodo generateResponse

--- Responder.java
+++ Responder.java
@@ -14,19 +14,31 @@ public class Responder
         aleatorio = new Random();
         responses = new ArrayList();
         respuestaAdecuada = new HashMap<String, String>();
-        
+
         responses.add(" 1");
         responses.add(" 2");
         responses.add(" 3");
         responses.add(" 4");
         responses.add(" 5");
-        
+
         respuestaAdecuada.put("servicio", "Ofrecemos varios servicios.");
         respuestaAdecuada.put("plataformas", "Diseño Multiplataforma.");
         respuestaAdecuada.put("fallos", "Error seran reportados");
         respuestaAdecuada.put("gratuito", " Software de prueba.");
-        
+
     }
 
+    public String generateResponse(String userInput)
+    {
+        String response = null;
+        response = respuestaAdecuada.get(userInput);
+
+        if (response == null){
+            response = responses.get(aleatorio.nextInt(responses.size()));
+        }
+
+        return response;
+    }
 }
-  
+
+
