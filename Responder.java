dcb1ae0c748d00d79170f3a4f61ff99cdec2abe8
David <david.clares.rodriguez@gmail.com>
2014-03-28 13:01:14 +0100

    Modificado contructor responder

--- Responder.java
+++ Responder.java
@@ -1,25 +1,32 @@
-/**
- * The responder class represents a response generator object.
- * It is used to generate an automatic response to an input string.
- * 
- * @author     Michael Kölling and David J. Barnes
- * @version    0.1 (2011.07.31)
- */
+import java.util.Random;
+import java.util.ArrayList;
+import java.util.HashMap;
 public class Responder
 {
+    Random aleatorio;
+    ArrayList<String> responses;
+    HashMap<String, String> respuestaAdecuada;
     /**
      * Construct a Responder - nothing to do
      */
     public Responder()
     {
+        aleatorio = new Random();
+        responses = new ArrayList();
+        respuestaAdecuada = new HashMap<String, String>();
+        
+        responses.add(" 1");
+        responses.add(" 2");
+        responses.add(" 3");
+        responses.add(" 4");
+        responses.add(" 5");
+        
+        respuestaAdecuada.put("servicio", "Ofrecemos varios servicios.");
+        respuestaAdecuada.put("plataformas", "Diseño Multiplataforma.");
+        respuestaAdecuada.put("fallos", "Error seran reportados");
+        respuestaAdecuada.put("gratuito", " Software de prueba.");
+        
     }
 
-    /**
-     * Generate a response.
-     * @return   A string that should be displayed as the response
-     */
-    public String generateResponse()
-    {
-        return "That sounds interesting. Tell me more...";
-    }
 }
+  
